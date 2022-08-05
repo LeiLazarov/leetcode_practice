2f47bb2b79fb9b3cf9f0509f702b9d179a83e472
Fizz Cao <stoneiiilei@gmail.com>
2022-08-06 01:12:00 +1000

    daily practice

---
+++ top_interview_questions/python/240_search_2D_matrix_II.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: Fizz Cao 
+@file: 242_valid_anagram.py
+@time: 6/08/2022 01:00 am
+'''
+
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        row, col = 0, len(matrix[0]) - 1
+        
+        while row < len(matrix) and col >= 0:
+            if matrix[row][col] < target:
+                row += 1
+            elif matrix[row][col] > target:
+                col -= 1
+            else:
+                return True
+        
+        return False
